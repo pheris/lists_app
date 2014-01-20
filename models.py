e1247509028ceab3d79faa6da13bfd2be35b5456
Fares Alhassen <falhassen@gmail.com>
2014-01-19 16:39:02 -0800

    Finished reference list

--- models.py
+++ models.py
@@ -3,15 +3,16 @@ import re
 import utils
 
 class Storeable(object):
-  """Stores an object into a dataMap.
+  """Stores an object into a dataMap."""
+  dataMap_ = {}
+
+  def __init__(self, key, data):
+    """Initializes a storeable object.
 
     Args:
         key: Object's key.
         data: Object's data.
-  """
-  dataMap_ = {}
-
-  def __init__(self, key, data):
+    """
     self.__class__.dataMap_[key] = data
 
   @classmethod
@@ -23,15 +24,23 @@ class Storeable(object):
     return len(self.dataMap_)
 
 class Character(Storeable):
-  """Stores a character's attributes.
-
-    Args:
-        name: A string containing the character's name.
-  """
+  """Stores a character's attributes."""
 
   def __init__(self, name):
+    """Initializes a character.
+
+      Args:
+          name: A string containing the character's name.
+    """
     self.name = name
+    if (self.name in self.__class__.dataMap_.keys()):
+      self.mentions_ = self.__class__.dataMap_[self.name].mentions_ + 1
+    else:
+      self.mentions_ = 1
     super(Character, self).__init__(self.name, self)
+
+  def getMentions(self):
+    return self.__class__.dataMap_[self.name].mentions_
 
   def __eq__(self, other):
     if isinstance(other, Character):
@@ -41,31 +50,65 @@ class Character(Storeable):
 class Reference(Storeable):
   """Stores a reference to a set of verses in a chapter."""
 
-  def __init__(self, reference_text, characters):
+  def __init__(self, text, characters):
     """ Initializes a reference.
 
     Args:
-        reference_text: A string contianing verse numbers and ranges, like
+        text: A string contianing verse numbers and ranges, like
             "1:2, 4, 5-7".
-        characters: An array of Character objects.
+        characters: An array of Character objects mentioned in the verse ranges.
     """
-    self.reference_text = reference_text
+    self.text = text
     self.characters = characters
-    chapter_text, verses_text = re.split(":", self.reference_text)
+    chapter_text, verses_text = re.split(':', self.text)
     self.chapter = int(chapter_text)
     self.verses = []
-    for verse_ranges in re.split(", ", verses_text):
+    for verse_ranges in re.split(r',\s', verses_text):
       if utils.is_number(verse_ranges):
         self.verses.append(int(verse_ranges))
       else:
-        start_text, end_text = re.split("-", verse_ranges)
+        start_text, end_text = re.split('-', verse_ranges)
         start = int(start_text)
         end = int(end_text) + 1
         self.verses += range(start, end)
-    super(Reference, self).__init__(self.reference_text, self)
+    super(Reference, self).__init__(self.text, self)
 
   def __eq__(self, other):
     if isinstance(other, Reference):
       return (self.chapter == other.chapter and
               self.verses == other.verses)
+    return NotImplemented
+
+class ReferenceList(Storeable):
+  """Stores a reference list."""
+
+  def __init__(self, text):
+    """ Initializes a reference list.
+
+    Args:
+        text: A string containing a reference list.
+    """
+    self.text = text
+    reference_texts = re.split(r';\s', self.text)
+    self.references = []
+    self.characters = {}
+    regex = re.compile(r'(\d)\s(\w)', re.UNICODE)
+    for complete_reference_text in reference_texts:
+      groups = regex.split(complete_reference_text)
+      reference_text = groups[0] + groups[1]
+      characters_text = groups[2] + groups[3]
+      names = re.split(',\s', characters_text)
+      group_characters = []
+      for name in names:
+        new_character = Character(name)
+        group_characters.append(new_character)
+        self.characters[name] = new_character
+      self.references.append(Reference(reference_text,
+        group_characters))
+    super(ReferenceList, self).__init__(self.text, self)
+
+  def __eq__(self, other):
+    if isinstance(other, Reference):
+      return (self.references == other.references and
+              self.characters == other.characters)
     return NotImplemented
--- testModels.py
+++ testModels.py
@@ -29,6 +29,13 @@ class TestCharacter(unittest.TestCase):
     new_character_b = models.Character('b nāme')
     self.assertEqual('b nāme', new_character_b.name)
 
+  def test_mentions(self):
+    john = models.Character('John')
+    self.assertEqual(1, john.getMentions())
+    john_copy = models.Character('John')
+    self.assertEqual(2, john.getMentions())
+    self.assertEqual(2, john_copy.getMentions())
+
   def test_eq(self):
     alice = models.Character('Ālice')
     bob = models.Character('Bôb')
@@ -45,9 +52,9 @@ class TestCharacter(unittest.TestCase):
 
 class TestReference(unittest.TestCase):
 
-  def assert_new_reference(self, reference_text, chapter, verses, characters):
-    new_reference = models.Reference(reference_text, characters)
-    self.assertEqual(new_reference.reference_text, reference_text)
+  def assert_new_reference(self, text, chapter, verses, characters):
+    new_reference = models.Reference(text, characters)
+    self.assertEqual(new_reference.text, text)
     self.assertEqual(new_reference.chapter, chapter)
     self.assertEqual(new_reference.verses, verses)
     self.assertEqual(new_reference.characters, characters)
@@ -86,31 +93,34 @@ class TestReference(unittest.TestCase):
     self.assertEqual(reference_c, models.Reference.get('11:13, 14, 15'))
     self.assertEqual(reference_d, models.Reference.get('112:1-3'))
 
-# class TestCharacterGroup(unittest.TestCase):
+class TestReferenceList(unittest.TestCase):
 
-#   def test_new(self):
-#     alice = models.Character('Ālice')
-#     bob = models.Character('Bôb')
-#     expected_references = [models.Reference('101:12-14'), models.Reference('9:12')]
-#     new_character = models.Character(expected_name)
-#     self.assertEqual(expected_name, new_character.name)
-#     self.assertEqual(expected_references, new_character.references)
-
-# class TestCharacterList(unittest.TestCase):
-
-#   def assert_new_character_list(self, list_text, chapter, character_verse_map):
-#     new_list = models.CharacterList(list_text)
-#     for i, character in enumerate(new_list.characters):
-#       self.assertEqual(character, characters[i])
-#     self.assertEqual(new_list.reference.chapter, chapter)
-#     self.assertEqual(new_list.reference.verses, verses)
-
-#   def test_new(self):
-#     self.assert_new_character_list((u'2:30-38 angels, Ādam; 2:34, 2:36 Iblīs;'
-#                                     u'2:35-36, 2:38 Ādam’s wife'),
-#                                     2,
-#                                     [12])
-
+  def test_new(self):
+    simple_list_text = '2:30-38 angels, Ādam; 2:34, 36 Iblīs; 2:35-36, 38 Ādam’s wife'
+    simple_reference_list = models.ReferenceList(simple_list_text)
+    angels_reference = simple_reference_list.references[0]
+    self.assertEqual(simple_reference_list.characters['angels'],
+      models.Character('angels'))
+    self.assertEqual(simple_reference_list.characters['Ādam'],
+      models.Character('Ādam'))
+    self.assertEqual(simple_reference_list.characters['Iblīs'],
+      models.Character('Iblīs'))
+    self.assertEqual(simple_reference_list.characters['Ādam’s wife'],
+      models.Character('Ādam’s wife'))
+    self.assertEqual(angels_reference.chapter, 2)
+    self.assertEqual(angels_reference.verses, range(30, 39))
+    self.assertEqual(angels_reference.characters[0],
+      models.Character('angels'))
+    self.assertEqual(angels_reference.characters[1], models.Character('Ādam'))
+    iblis_reference = simple_reference_list.references[1]
+    self.assertEqual(iblis_reference.chapter, 2)
+    self.assertEqual(iblis_reference.verses, [34, 36])
+    self.assertEqual(iblis_reference.characters[0], models.Character('Iblīs'))
+    adam_wife_reference = simple_reference_list.references[2]
+    self.assertEqual(adam_wife_reference.chapter, 2)
+    self.assertEqual(adam_wife_reference.verses, [35, 36, 38])
+    self.assertEqual(adam_wife_reference.characters[0],
+      models.Character('Ādam’s wife'))
 
 if __name__ == '__main__':
   unittest.main()
